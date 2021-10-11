9c391144d58a1a2d5c005f71695e4588c7a5de7f
gatsby003 <ganeshfutane3@gmail.com>
2021-10-11 15:03:50 +0530

    added option icons , and transitons

--- src/components/MidBar/Time.js
+++ src/components/MidBar/Time.js
@@ -1,13 +1,12 @@
 import Clock from 'react-live-clock'
-import {H} from 'react-headings'
 import style from '../../styles.module.css'
 
 const Time = () => {
     return (
-        <div style={{display : "flex", alignSelf : 'center'}}>
-        <H style={{margin : "0", color: "white", fontSize: "120px", fontWeight: "100" ,fontFamily: "monospace"}}>
-          <Clock style={{letterSpacing : "-0.1em"}} format="h:mm" interval={1000}/>
-        </H>
+        <div style={{display : "flex", alignSelf : 'center', paddingLeft : "25px", paddingRight : "25px"}}>
+        <h1 style={{margin : "0", color: "white", fontSize: "120px", fontWeight: "100" ,fontFamily: "monospace"}}>
+          <Clock style={{letterSpacing : "-0.1em"}} format="h:mm" interval={1000} ticking={true}/>
+        </h1>
         </div>
     )
 }
--- src/components/MidBar/TimeAndGreeting.js
+++ src/components/MidBar/TimeAndGreeting.js
@@ -1,13 +1,38 @@
-import React from "react";
+import React, { useState }from "react";
 import Time from "./Time";
 import Greeting from "./Greeting";
 import styles from '../../styles.module.css'
+import AccessTimeIcon from '@mui/icons-material/AccessTime';
+import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
+import Button from '@mui/material/Button';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
 
 const TimeAndGreeting = () => {
+    const [leftstyle, setRightStyle] = useState(styles.clock_left_hidden)
+    const [rightstyle, setLeftStyle] = useState(styles.clock_right_hidden)
+    const [showMenu, setShowMenu] = useState(false)
 
     return (
         <div className={styles.centerBar}>
-            <Time/>
+            <div className={styles.clock}
+                onMouseEnter={e => {
+                    setLeftStyle(styles.clock_left_active)
+                    setRightStyle(styles.clock_right_active)
+                }}
+                onMouseLeave={e => {
+                    setLeftStyle(styles.clock_left_hidden)
+                    setRightStyle(styles.clock_right_hidden)
+                }}
+            >
+                <div className={leftstyle}>
+                    <AccessTimeIcon onClick= {() => setShowMenu(!(showMenu))}/>
+                </div>
+                <Time/>
+                <div className={rightstyle}>
+                    <MoreHorizIcon/>
+                </div>
+            </div>
             <Greeting/>
         </div>
     )
